c88e2e8c274ddda04efb8e7be1c2ed6d70cfcbe2
llufan <llufan2796@gmail.com>
2017-07-26 16:13:05 -0400

    user authentication

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// db setup
+mongoose.connect('mongodb://localhost:auth/auth');
+// app
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// server
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
---
+++ models/dish.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const dishSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  by: { type: Schema.Types.ObjectId, ref: 'user' }
+});
+
+const modelClass = mongoose.model('dish', dishSchema);
+
+module.exports = modelClass;
