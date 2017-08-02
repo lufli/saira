00c8314fa1d5097c2d31ae2a9dfbefcba803e7ae
yohannli2015 <yohannli2015@gmail.com>
2017-08-01 22:05:05 -0400

    search dish
    search dish

--- saira-master/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secret: 'mingnianshangshi',
-  // e.g. https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyD_vES1GzjKJ4pdVuQBRRnbxAB1ZGKdk7U
-  mapKey: 'AIzaSyD_vES1GzjKJ4pdVuQBRRnbxAB1ZGKdk7U'
-}
